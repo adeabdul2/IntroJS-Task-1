837c915ab3ceda595c35512b045daca7800ece69
Abdulafeez Adesokan <adeshokan.a@gmail.com>
2022-06-25 14:51:13 +0100

    IntroJS Task 1
    This is a small JavaScript task to print an output statement on the screen.

---
+++ IntroJS-1.js
@@ -0,0 +1,12 @@
+const names = "Abdulafeez Adesokan";
+const height = "5feet 7 inches";
+const country = "Nigeria";
+
+function aboutMe(){
+    
+    console.log("Hi! My name is " + names + ". I am " + height + " tall and I am from Lagos, " + country + ".");
+    alert("Hi! My name is " + names + ". I am " + height + " tall and I am from Lagos, " + country + ".");
+
+}
+
+aboutMe()
